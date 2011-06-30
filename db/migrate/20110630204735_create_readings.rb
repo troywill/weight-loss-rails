35e79edc2e2eaa12f4697b256e7aa7f43fdc66c1
Troy Will <troydwill@gmail.com>
2011-06-30 15:46:46 -0700

    new file:   ../db/migrate/20110630204735_create_readings.rb

---
+++ db/migrate/20110630204735_create_readings.rb
@@ -0,0 +1,15 @@
+class CreateReadings < ActiveRecord::Migration
+  def self.up
+    create_table :readings do |t|
+      t.integer :user_id
+      t.decimal :weight
+      t.datetime :reading_time
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :readings
+  end
+end
