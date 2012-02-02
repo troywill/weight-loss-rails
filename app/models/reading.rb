7af9636cbeb68d9c4c2d5e18bfca936ea8f8794f
Troy Will <troydwill@gmail.com>
2012-02-01 22:48:37 -0800

    Delete some files

--- app/models/reading.rb
+++
@@ -1,8 +0,0 @@
-class Reading < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, :numericality => true
-  validates :weight, :numericality => true
-
-  scope :by_user, lambda { |user_id| where('user_id == ?', user_id) }
-  
-end
--- app/models/setting.rb
+++
@@ -1,7 +0,0 @@
-class Setting < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, :presence => true, :numericality => true
-  validates :weight_loss_rate, :numericality => true
-
-  scope :by_user, lambda { |user_id| where('user_id == ?', user_id) }
-end
