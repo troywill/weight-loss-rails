2738bc0def8c1e8af86a49b4709623354bec2241
Troy Will <troydwill@gmail.com>
2012-03-09 13:11:48 -0800

    010_controller_welcome.sh

--- 010_controller_welcome.sh
+++ 010_controller_welcome.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+
 # <troydwill@gmail.com> 2012-02-01
 source ./lib-rrp
 rails generate controller welcome index login logout
+sleep 5
 ${EDITOR} ${TOP_DIR}/config/routes.rb
 mv -iv ${TOP_DIR}/public/index.html /tmp/
