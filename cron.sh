c85d9712d18ba4fa71febf3c1551e6bf6ae067c9
Troy Will <troydwill@gmail.com>
2012-03-17 10:41:46 -0700

    STEP_BY_STEP

---
+++ cron.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+cd /srv/rails/troy/script/troy/stand_alone
+./00.rb
---
+++ stand_alone/00.rb
@@ -0,0 +1,4 @@
+#!/usr/local/bin/ruby-1.9.3-p125/ruby
+require "../../../config/environment.rb"
+user_id = 1
+GoalReminder.goal.deliver
