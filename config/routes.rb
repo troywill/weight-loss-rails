905adcd40736af13a1226b91735b50ca40f4fde3
Troy Will <troydwill@gmail.com>
2012-03-28 17:19:44 -0700

    new file:   config/routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Troy::Application.routes.draw do
+  root :to => 'welcome#index'
+
+  resources :settings
+  resources :readings
+  resources :users
+  
+  get "welcome/index"
+  get "welcome/graph"
+  get "welcome/login"
+  post "welcome/login"
+  get "welcome/logout"
+
+  get "weight_graph/week"
+  get "weight_graph/month"
+  get "weight_graph/year"
+
+end
