42d46c05a3722952a18df0627c948cb1a39cf8db
Troy Will <troydwill@gmail.com>
2012-03-28 21:34:11 -0700

    modified:   ../../controllers/weight_graph_controller.rb

--- app/controllers/weight_graph_controller.rb
+++ app/controllers/weight_graph_controller.rb
@@ -29,5 +29,27 @@ class WeightGraphController < ApplicationController
   end
 
   def year
+    g = Gruff::Line.new
+    weight = 0
+    time_at_point_in_past = 0
+    user_id = session[:user_id]
+    time_first_reading = Reading.time_initial(user_id)
+    weight_first_reading = Reading.weight_initial(user_id).to_f
+    # Get weight values for last 12 days
+    weight_array = Array.new
+    number_of_periods = 12
+    (0..number_of_periods).each do |period_num|
+      time_at_point_in_past = Time.now-(number_of_periods-period_num).month
+
+      if ( time_at_point_in_past < time_first_reading )
+        weight = weight_first_reading
+      else
+        weight = Reading.weight_at_time(user_id, time_at_point_in_past)
+      end
+      weight_array.push(weight)
+    end
+
+    g.data "Last 12 months", weight_array
+    send_data(g.to_blob, :type => 'image/png')
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -59,29 +59,6 @@ class WelcomeController < ApplicationController
   end
 
   def one_year_graph
-    g = Gruff::Line.new
-    weight = 0
-    time_at_point_in_past = 0
-    user_id = session[:user_id]
-    time_first_reading = Reading.time_initial(user_id)
-    weight_first_reading = Reading.weight_initial(user_id).to_f
-    # Get weight values for last 2 years
-    weight_array = Array.new
-    number_of_periods = 24
-    (0..number_of_periods).each do |period_num|
-      time_at_point_in_past = Time.now-(number_of_periods-period_num).month
-
-      if ( time_at_point_in_past < time_first_reading )
-        weight = weight_first_reading
-      else
-        weight = Reading.weight_at_time(user_id, time_at_point_in_past)
-      end
-      weight_array.push(weight)
-    end
-
-    g.data "FOO", weight_array
-    send_data(g.to_blob, :type => 'image/png', :disposition => 'inline' )
-#    send_data(g.to_blob, :type => 'image/png', :filename => "two_year.png", )
   end
 
 end
