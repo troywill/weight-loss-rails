5c10e2d5afe2e389d09a5ec51d3ce45615a67073
Troy Will <troydwill@gmail.com>
2013-05-09 05:42:42 +0400

    modified:   ../step-by-step-weight-loss.org

---
+++ bin/generate-scaffold-setting.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rails generate scaffold setting user_id:integer \
+    filter_rate_gain:integer \
+    filter_rate_loss:integer \
+    custom_graph:boolean \
+    graph_upper:integer \
+    graph_lower:integer \
+    graph_lines:integer \
+    si:boolean \
+    clothing:boolean \
+    clothing_wt:decimal\
+    timezone:integer \
+    locale:string \
+    --force
